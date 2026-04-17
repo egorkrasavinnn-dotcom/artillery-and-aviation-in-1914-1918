cc87b00df68f2187d98fc4923ab45e385085d664
egorkrasavinnn <egorkrasavinnn@gmail.com>
2026-04-17 13:48:57 +0300

    Fix next.config - remove duplicate config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Отключаем Turbopack на время, чтобы билд стабильно работал
+  experimental: {
+    turbopack: false
+  }
 }
 
 module.exports = nextConfig
